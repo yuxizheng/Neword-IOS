a5385734771aae0f8cc6735f702803513deb9721
yuxizheng <zhengyuxi2000@163.com>
2022-11-12 15:19:39 +0800

    Initial Commit

---
+++ Neword/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neword
+//
+//  Created by 郑语悉 on 2022/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neword/NewordApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewordApp.swift
+//  Neword
+//
+//  Created by 郑语悉 on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct NewordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
